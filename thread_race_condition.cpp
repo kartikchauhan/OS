de18f9a79e9d9616d69c0170ffac9721610f6249
kartikchauhan <chauhan.kartik25@gmail.com>
2023-04-07 00:43:56 +0530

    race condition in threads

---
+++ thread_race_condition.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <thread>
+#include <unistd.h>
+
+using namespace std;
+
+int counts = 0;
+
+void task() {
+    for (int i = 0; i < 10000; i++) {
+        counts++;
+    }    
+}
+
+int main() {
+    thread t1(task);
+    thread t2(task);
+
+    t1.join();
+    t2.join();
+
+    cout << counts << endl; // prints 14933
+
+    return 0;
+}
